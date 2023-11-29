538c0264b9ace8932d057aca51baebc493dd8ee7
06hyunmine <148820836+06hyunmine@users.noreply.github.com>
2023-11-29 14:24:48 +0900

    Add files via upload

---
+++ 제곱.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+main(){
+	double number1,number2;
+ printf("숫자입력:");
+ scanf("%lf %lf", &number1, &number2);
+ printf("%lf",pow(number1, number2));
+	
+	
+	
+	
+}
